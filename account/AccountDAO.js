6e79549bac99cd24c9a2386a902354664e5bc6f8
Th7mo <thimo@th7mo.nl>
2022-05-25 13:13:16 +0200

    Changed that query's now and with a ';'

--- account/AccountDAO.js
+++ account/AccountDAO.js
@@ -6,7 +6,7 @@ module.exports = class AccountDAO {
     static createAccount(account) {
         const query = `
             INSERT INTO account (id, username, email_address, hashed_password)
-            VALUES ($1, $2, $3, $4)
+            VALUES ($1, $2, $3, $4);
         `;
         const values = [
             uuidv4(),
@@ -21,7 +21,7 @@ module.exports = class AccountDAO {
     static deleteAccount(emailAddress) {
         const query = `
             DELETE FROM account
-            WHERE email_address = ($1)
+            WHERE email_address = ($1);
         `;
         const values = [
             emailAddress
