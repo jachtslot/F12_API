6bce065e1e90541d9aa38ef261216ac32cfaa25b
Th7mo <thimo@th7mo.nl>
2022-06-01 14:48:45 +0200

    Made postgresqladapter have a seperate method for executing query's without values

--- util/PostgreSQLAdapter.js
+++ util/PostgreSQLAdapter.js
@@ -3,8 +3,13 @@ const databaseCredentials = require('./DatabaseCredentials').CREDENTIALS
 
 module.exports = class PostgreSQLAdapter {
 
-    static async executeQuery({query, values}) {
+    static async executeQueryWithValues({query, values}) {
         const pool = new Pool(databaseCredentials);
         return await pool.query(query, values);
+    }
+
+    static async executeQuery(query) {
+        const pool = new Pool(databaseCredentials);
+        return await pool.query(query);
     }
 }
