d4d205eb289cf9cfea38e479abf3593e4e9d0346
RamonK <ramonkrijnen10@outlook.com>
2022-06-04 21:39:28 +0200

    Created AuthenticationHelper that handles various methods in the signup process.

---
+++ util/AuthenticationHelper.js
@@ -0,0 +1,39 @@
+const Account = require('../account/Account');
+const jwt = require('jsonwebtoken');
+module.exports = class AuthenticationHelper {
+
+    static parseBody(event) {
+        let body = JSON.parse(event.body);
+
+        const email = body.email_address;
+        const password = body.hashed_password;
+        return {email, password};
+    }
+
+
+    static createUserFromData(data) {
+        if (!data.rows[0]) {
+            throw new Error("No account connected to that emailaddress", 401)
+        } else {
+            data = data.rows[0];
+            return new Account(
+                data.id,
+                data.username,
+                data.email_address,
+                data.hashed_password
+            );
+        }
+    }
+
+    static generateToken(account) {
+        return jwt.sign(
+            {
+                id: account.id,
+                email: account.emailAddress
+            },
+            process.env.JWT_SECRET, {
+                expiresIn: '1h'
+            }
+        )
+    }
+}
