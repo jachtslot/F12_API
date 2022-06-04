f743242cd139c2cb8f2912de4e2a9b248093554c
RamonK <ramonkrijnen10@outlook.com>
2022-06-04 15:44:59 +0200

    Refactored generation of account ID
    the generating of account id is now handled when creating a new Account in the constructor.

--- account/Account.js
+++ account/Account.js
@@ -1,8 +1,12 @@
+const {v4: uuidv4} = require('uuid');
+
 module.exports = class Account {
 
     constructor(username, emailAddress, hashedPassword) {
+        this.id = uuidv4();
         this.username = username;
         this.emailAddress = emailAddress;
         this.hashedPassword = hashedPassword;
     }
+
 }
