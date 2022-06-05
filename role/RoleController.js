1a688fc518ac94c166c8a973df56cc704f8382b3
th7mo <dev@th7mo.com>
2022-06-05 14:57:07 +0200

    Added check that Rolename must be uniqie

--- role/RoleController.js
+++ role/RoleController.js
@@ -3,6 +3,12 @@ const RoleDAO = require('./RoleDAO');
 module.exports = class RoleController {
 
     static async createRole(roleName) {
-        return RoleDAO.createRole(roleName);
+        return RoleDAO.getRole(roleName).then(role => {
+            if (role.rows.length > 0) {
+                throw new Error(`Role with name '${roleName}' already exists!`);
+            }
+
+            return RoleDAO.createRole(roleName);
+        });
     }
 }
