4ea84df59bdef6c585a1da0f9f44dc4cffcd5a15
Th7mo <thimo@th7mo.nl>
2022-06-05 15:58:21 +0200

    Added jasmine dependency for unit-testing

--- role/RoleController.js
+++ role/RoleController.js
@@ -3,7 +3,7 @@ const RoleDAO = require('./RoleDAO');
 module.exports = class RoleController {
 
     static async createRole(roleName) {
-        let existingRoles = RoleDAO.getRole(roleName);
+        let existingRoles = await RoleDAO.getRole(roleName);
 
         if (existingRoles.length > 0) {
             throw new Error(`Role with name '${roleName}' already exists!`);
