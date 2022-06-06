a4a83c642c95ef708929252a124381c300b10e25
RamonK <ramonkrijnen10@outlook.com>
2022-06-06 15:35:21 +0200

    Created template class for sending email credentials
    * Refactored EmailHelper to new situation

--- util/EmailHelper.js
+++ util/EmailHelper.js
@@ -1,5 +1,6 @@
 const AWS = require('aws-sdk');
 require('dotenv').config();
+const formatEmail = require('./EmailTemplate');
 
 const SES_CONFIG = {
     accessKeyId: process.env.SES_ACCESS_KEY,
@@ -10,32 +11,8 @@ const SES_CONFIG = {
 const AWS_SES = new AWS.SES(SES_CONFIG);
 
 const sendRegistrationEmail = async (email, password) => {
-    let params = {
-        Source: process.env.EMAIL_SENDER,
-        Destination: {
-            ToAddresses: [
-                email
-            ],
-        },
-        ReplyToAddresses: [],
-        Message: {
-            Body: {
-                Html: {
-                    Charset: 'UTF-8',
-                    Data: 'Beste, <br>' +
-                        'Vanaf nu is het mogelijk om op ingestelde tijdstippen het landgoed Ter Horsten te betreden. <br>' +
-                        'Met de bijgevoegde inloggegevens kan via de app {{LINK}} toegang worden verkregen<br>' +
-                        `emailadres: ${email}<br>
-                        wachtwoord: ${password}`,
-                },
-            },
-            Subject: {
-                Charset: 'UTF-8',
-                Data: `Toegang tot Landgoed Ter Horst`,
-            }
-        },
-    };
-    return await AWS_SES.sendEmail(params).promise();
+    const template = formatEmail(email, password)
+    return await AWS_SES.sendEmail(template).promise();
 }
 
 module.exports = sendRegistrationEmail;
