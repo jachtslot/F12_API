464a93878fd4f3f162d076e44a5cd085e815b910
RamonK <ramonkrijnen10@outlook.com>
2022-06-06 18:05:43 +0200

    Moved response handling to AuthenticationHandler

--- authentication/AuthenticationController.js
+++ authentication/AuthenticationController.js
@@ -20,27 +20,7 @@ module.exports = class AuthenticationController {
                     throw new Error("Wrong password", 401);
                 }
                 const token = AuthenticationHelper.generateToken(loadedAccount);
-
-                return {
-                    statusCode: 200,
-                    headers: {
-                        "Access-Control-Allow-Headers": "Content-Type",
-                        "Access-Control-Allow-Origin": "http://localhost:4200",
-                        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-                    },
-                    body: JSON.stringify(
-                        {
-                            token: token,
-                            id: loadedAccount.id,
-                            email_address: loadedAccount.emailAddress,
-                            username: loadedAccount.username
-                        })
-                }
-            }).catch(error => {
-                return {
-                    statusCode: 500,
-                    body: error.message
-                }
+                return {loadedAccount, token};
             });
     }
 }
