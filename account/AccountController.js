94f7a0c1eab607bb8b06a2a925a2e1e199f82fbe
RamonK <ramonkrijnen10@outlook.com>
2022-06-07 13:39:32 +0200

    Moved hashing of password to AccountController. This allows for unit testing of the functionality.

--- account/AccountController.js
+++ account/AccountController.js
@@ -1,19 +1,23 @@
 const AccountDAO = require('./AccountDAO');
 const sendRegistrationMail = require('../util/EmailHelper');
-
+const bcrypt = require('bcryptjs');
 
 
 module.exports = class AccountController {
 
     static async createAccount(account) {
+        await bcrypt.hash(account.hashedPassword, 12).then(hashedPw => {
+            account.hashedPassword = hashedPw;
+            return account;
+        });
         return await AccountDAO.createAccount(account)
             .then(() => {
+                console.log(account)
                 sendRegistrationMail(account.emailAddress, account.hashedPassword).catch(error => {
                     throw new Error(error.message);
                 })
                 return account;
             })
-
     }
 
     static async deleteAccount(emailAddress) {
