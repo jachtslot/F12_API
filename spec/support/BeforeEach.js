090af7d708a6f8e26a453afaf1783872abefcfcc
th7mo <dev@th7mo.com>
2022-06-08 14:02:53 +0200

    made jasmine timeout longer

--- spec/support/BeforeEach.js
+++ spec/support/BeforeEach.js
@@ -4,6 +4,7 @@ const PostgreSQLAdapter = require("../../util/PostgreSQLAdapter");
 module.exports = class BeforeEach {
 
     static async run() {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000000;
         databaseCredentials.host = process.env.POSTGRES_HOST_TEST_ADDRESS;
         process.env.EMAIL_SENDER = '';
         await PostgreSQLAdapter.clearAllTables();
