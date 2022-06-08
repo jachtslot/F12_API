def0daf041820217381d4078a28a209cba72d0d5
th7mo <thimo@th7mo.nl>
2022-06-08 13:49:39 +0200

    Added more unit tests to addAccountToRole method

--- spec/role/RoleController.test.js
+++ spec/role/RoleController.test.js
@@ -2,9 +2,16 @@ const Account = require('../../account/Account');
 const AccountController = require('../../account/AccountController');
 const RoleController = require('../../role/RoleController');
 const BeforeEach = require('../support/BeforeEach');
+const { v4: uuidv4 } = require('uuid');
 
 const genRandomNumber = () => {
     return Math.floor(Math.random().toPrecision(9) * 1_000_000_000);
+}
+
+const insertRole = async (roleName) => {
+    await RoleController.createRole(roleName);
+    let role = await RoleController.getRole(roleName);
+    return role.id;
 }
 
 const insertAccount = async () => {
@@ -58,9 +65,7 @@ describe('testing the deleteRole method of the RoleController()', () => {
 
     it('Deletes a record in the Role table', async () => {
         await BeforeEach.run();
-        await RoleController.createRole('tuinman');
-        let roles = await RoleController.getRole('tuinman');
-        let roleId = roles.id;
+        let roleId = await insertRole('tuinman');
 
         await RoleController.deleteRole(roleId);
         let roleRecords = await RoleController.getAllRoles();
@@ -78,11 +83,9 @@ describe('testing the deleteRole method of the RoleController()', () => {
 
     it('Deletes only one record in the Role table', async () => {
         await BeforeEach.run();
-        await RoleController.createRole('tuinman');
         await RoleController.createRole('different');
         await RoleController.createRole('another');
-        let roles = await RoleController.getRole('tuinman');
-        let roleId = roles.id;
+        let roleId = await insertRole('tuinman');
 
         await RoleController.deleteRole(roleId);
         let roleRecords = await RoleController.getAllRoles();
@@ -95,31 +98,19 @@ describe('testing the addAccountToRole method of the RoleController()', () => {
 
     it('adds a account to the role if existing', async () => {
         await BeforeEach.run();
-        await RoleController.createRole('tuinman');
-        let role = await RoleController.getRole('tuinman');
-        let roleId = role.id;
+        let roleId = await insertRole('tuinman');
 
-        let newAccount = new Account(
-            null,
-            'example_user_name',
-            'example@gmail.com',
-            'example_hashed_password'
-        );
+        let accountId1 = await insertAccount();
 
-        let createdAccount = await AccountController.createAccount(newAccount);
-        let accountId = createdAccount.id;
-
-
-        await RoleController.addAccountToRole(roleId, accountId);
+        await RoleController.addAccountToRole(roleId, accountId1);
         let roleWithAccount = await RoleController.getRole('tuinman');
+
         expect(roleWithAccount.accounts.length).toBe(1);
     });
 
     it('adds two accounts to the role if existing', async () => {
         await BeforeEach.run();
-        await RoleController.createRole('tuinman');
-        let role = await RoleController.getRole('tuinman');
-        let roleId = role.id;
+        let roleId = await insertRole('tuinman');
 
         let accountId1 = await insertAccount();
         let accountId2 = await insertAccount();
@@ -128,7 +119,38 @@ describe('testing the addAccountToRole method of the RoleController()', () => {
         await RoleController.addAccountToRole(roleId, accountId2);
         let roleWithAccount = await RoleController.getRole('tuinman');
 
-        console.log(roleWithAccount);
         expect(roleWithAccount.accounts.length).toBe(2);
+    });
+
+    it('crashes when id of account not found in database', async () => {
+        await BeforeEach.run();
+        let roleId = insertRole('tuinman');
+        let accountId1 = uuidv4();
+
+        await expectAsync(
+            RoleController.addAccountToRole(roleId, accountId1)
+        ).toBeRejected();
+    });
+
+    it('crashes when id of role not found in database', async () => {
+        await BeforeEach.run();
+
+        let roleId = uuidv4();
+        let accountId1 = await insertAccount();
+
+        await expectAsync(
+            RoleController.addAccountToRole(roleId, accountId1)
+        ).toBeRejected();
+    });
+
+    it('crashes when id of role and account not found in database', async () => {
+        await BeforeEach.run();
+
+        let roleId = uuidv4();
+        let accountId1 = uuidv4();
+
+        await expectAsync(
+            RoleController.addAccountToRole(roleId, accountId1)
+        ).toBeRejected();
     });
 });
