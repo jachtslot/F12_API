867d00f9ce22c9318c447ff20219dfd5c120b79d
RamonK <ramonkrijnen10@outlook.com>
2022-06-13 14:35:01 +0200

    Formatting

--- permission/PermissionDAO.js
+++ permission/PermissionDAO.js
@@ -5,14 +5,9 @@ module.exports = class PermissionDAO {
     static addPermission(permission) {
         const query = `
             INSERT INTO permission (
-                    id,
-                    role_id,
-                    privilege_id,
-                    p_day,
-                    start_time,
-                    end_time
-                    )
-                   VALUES($1, $2, $3, $4, $5, $6)`;
+              id, role_id, privilege_id,
+              p_day, start_time, end_time)
+            VALUES($1, $2, $3, $4, $5, $6);`;
         const values = [
             permission.id,
             permission.role_id,
