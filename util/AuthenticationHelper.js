aff2192734415361a15f443e4f61a57c156ae0c3
RamonK <ramonkrijnen10@outlook.com>
2022-06-13 10:14:18 +0200

    Started working on a method that verifies JWT tokens

--- util/AuthenticationHelper.js
+++ util/AuthenticationHelper.js
@@ -35,4 +35,26 @@ module.exports = class AuthenticationHelper {
             }
         )
     }
+
+    verifyToken(event) {
+        let token = event.headers['Authorization'].split(' ')[1];
+        if (!token) {
+            const error = new Error('Not authenticated');
+            error.statusCode = 401;
+            throw error;
+        }
+        let decodedToken;
+        try {
+            decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        } catch (err) {
+            err.statusCode = 500;
+            throw err;
+        }
+        if (!decodedToken) {
+            const error = new Error('Not authenticated')
+            error.statusCode = 401;
+            throw error;
+        }
+
+    }
 }
