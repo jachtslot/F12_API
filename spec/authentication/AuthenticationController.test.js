9f48dcc99e56b2eee7d10e685666b9669876d8a5
RamonK <ramonkrijnen10@outlook.com>
2022-06-15 14:47:50 +0200

    Created tests for the login method

--- spec/authentication/AuthenticationController.test.js
+++ spec/authentication/AuthenticationController.test.js
@@ -3,42 +3,59 @@ const accountController = new AccountController();
 const AuthenticationController = require('../../authentication/AuthenticationController');
 const authenticationController = new AuthenticationController();
 const Account = require('../../account/Account');
+const PostgresqlAdapter = require('../../util/PostgreSQLAdapter');
 const BeforeEach = require('../support/BeforeEach');
 
 const beforeEach = async () => {
     await BeforeEach.run();
-    await accountController.createAccount(getTestAccount());
+    const testAdmin = getTestAdmin();
+    await accountController.createAccount(testAdmin);
+    await PostgresqlAdapter.executeQueryWithValues('INSERT INTO admin (account_id) VALUES($1)', [testAdmin.id]);
+    await accountController.createAccount(getTestUser());
+
 }
 
-const getTestAccount = () => {
+const getTestAdmin = () => {
     return new Account(
         null,
-        'dave',
-        's1127893@student.hsleiden.nl',
-        'Password'
+        'Wachtwoord',
+        'originelemail@hotmail.com',
+        'Wachtwoord'
+    )
+}
+const getTestUser = () => {
+    return new Account(
+        null,
+        'Wachtwoord',
+        'originelemail2@hotmail.com',
+        'Wachtwoord'
     )
 }
 
 describe('testing the login() method of the AuthenticationController', () => {
 
-    it('should return an object with user and access_token on successful login', async () => {
-        await beforeEach()
-        const result = await authenticationController.login(
-            {
-                email: getTestAccount().emailAddress,
-                password: getTestAccount().hashedPassword
-            }
-        )
-        expect(result.loadedAccount.username).toBe(getTestAccount().username);
-        expect(result.token.length).toBeGreaterThan(1);
-    });
+    it('should return an object with access_token on successful login to web portal with admin account',
+        async () => {
+            await beforeEach();
+            const result = await authenticationController.login(
+                {
+                    email: getTestAdmin().emailAddress,
+                    password: getTestAdmin().hashedPassword,
+                    origin: process.env.PORTAL_ORIGIN
+                }
+            )
+            expect(result.loadedAccount.username).toBe(getTestAdmin().username);
+            expect(result.token.length).toBeGreaterThan(1);
+        });
 
     it('should throw error when given incorrect password', async () => {
         await beforeEach();
         await expectAsync(authenticationController.login(
             {
-                email: getTestAccount().emailAddress,
-                password: 'incorrectPassword'
+                email: getTestAdmin().emailAddress,
+                password: 'incorrectPassword',
+                origin: process.env.PORTAL_ORIGIN
+
             }
         )).toBeRejected();
     });
@@ -48,8 +65,28 @@ describe('testing the login() method of the AuthenticationController', () => {
         await expectAsync(authenticationController.login(
             {
                 email: 'NonExistingEmail@hotmail.com',
-                password: getTestAccount().hashedPassword
+                password: getTestAdmin().hashedPassword,
+                origin: process.env.PORTAL_ORIGIN
             }
         )).toBeRejected();
+    });
+
+    it('should throw error when logging in to web portal as mobile account', async () => {
+        await beforeEach();
+        await expectAsync(authenticationController.login({
+                email: getTestUser().emailAddress,
+                password: getTestUser().hashedPassword,
+                origin: process.env.PORTAL_ORIGIN
+            }
+        )).toBeRejected();
+    });
+    it('should return an access token when logging to mobile app as admin', async () => {
+        await beforeEach();
+        await expectAsync(authenticationController.login({
+            email: getTestAdmin().emailAddress,
+            password: getTestAdmin().hashedPassword,
+            origin: process.env.APP_ORIGIN
+        })).toBeResolved();
+
     });
 });
