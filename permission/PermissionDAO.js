9078875989f7ee54545d23975063607ea3220a8f
th7mo <thimo@th7mo.nl>
2022-06-15 13:34:39 +0200

    moved order of WHERE check for consistency

--- permission/PermissionDAO.js
+++ permission/PermissionDAO.js
@@ -23,7 +23,7 @@ module.exports = class PermissionDAO {
         const GET_PERMISSION_BY_ROLE = `
             SELECT *
             FROM permission
-            WHERE $1 = role_id;
+            WHERE role_id = $1;
         `;
         const values = [roleId];
 
