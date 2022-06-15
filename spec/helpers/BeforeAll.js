d75de55dae9ea80a6a83425121d14188c925e595
th7mo <dev@th7mo.com>
2022-06-15 16:50:16 +0200

    changed that everybody now uses sql script to init private database for testing

---
+++ spec/helpers/BeforeAll.js
@@ -0,0 +1,17 @@
+const {CREDENTIALS: databaseCredentials} = require("../../util/DatabaseCredentials");
+const ONE_MINUTE_IN_MS = 60_000;
+
+beforeAll(async () => {
+    const fs = require('fs'),
+        path = require('path'),
+        filePath = path.join(__dirname, 'f12.sql');
+
+    console.log('setting up database');
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = ONE_MINUTE_IN_MS;
+    process.env.EMAIL_SENDER = '';
+    const { CREDENTIALS: databaseCredentials } = require("../../util/DatabaseCredentials");
+    databaseCredentials.database = process.env.POSTGRES_TEST_DATABASE;
+    const PostgreSQLAdapter = require("../../util/PostgreSQLAdapter");
+    const DATABASE_INIT = fs.readFileSync(filePath).toString();
+    await PostgreSQLAdapter.executeQuery(DATABASE_INIT);
+});
