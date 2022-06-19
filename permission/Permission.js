d81605602e863f1235cb96da6c831d4065ea0a0a
th7mo <dev@th7mo.com>
2022-06-19 15:22:37 +0200

    changed that permission can now have a given id, changed attributes to reflect database rows

--- permission/Permission.js
+++ permission/Permission.js
@@ -2,12 +2,12 @@ const {v4: uuidv4} = require('uuid');
 
 module.exports = class Permission {
 
-    constructor(role_id, privilege_id, day, begin_time, end_time) {
-        this.id = uuidv4();
+    constructor(id, role_id, privilege_id, p_day, start_time, end_time) {
+        this.id = id ? id : uuidv4()
         this.role_id = role_id;
         this.privilege_id = privilege_id;
-        this.day = day;
-        this.begin_time = begin_time;
+        this.p_day = p_day;
+        this.start_time = start_time;
         this.end_time = end_time;
     }
 }
