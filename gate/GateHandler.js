ac46c9ba3e381a6c08abb49ce92f8cb4a1e2bd5d
th7mo <dev@th7mo.com>
2022-06-20 15:13:40 +0200

    added error response for when user has no access to gate

--- gate/GateHandler.js
+++ gate/GateHandler.js
@@ -4,12 +4,21 @@ const AuthenticationHelper = require('../util/AuthenticationHelper');
 const RequestValidator = require('../util/RequestValidator');
 const Time = require('../util/Time');
 
+const ResponseFactory = require('../response/ResponseFactory');
+const Methods = require('../response/methods').Methods;
+
 module.exports.openInnerGate = async event => {
     const decodedToken = AuthenticationHelper.verifyToken(event);
     const accountId = decodedToken.id;
     const requestValidator = new RequestValidator(accountId, Time.now(), 1);
     if (await requestValidator.hasAccess()) {
         return gateController.openInnerGate(event);
+    } else {
+        return ResponseFactory.build(
+            403,
+            Methods.POST,
+            `account with id ${accountId} can't open inner gate at ${requestValidator.currentTime}`
+        );
     }
 }
 
@@ -19,5 +28,11 @@ module.exports.openOuterGate = async event => {
     const requestValidator = new RequestValidator(accountId, Time.now(), 2);
     if (await requestValidator.hasAccess()) {
         return gateController.openOuterGate(event);
+    } else {
+        return ResponseFactory.build(
+            403,
+            Methods.POST,
+            `account with id ${accountId} can't open outer gate at ${requestValidator.currentTime}`
+        );
     }
 }
