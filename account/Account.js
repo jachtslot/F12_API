91383bf47291d567ee24b16f2930919205f97359
th7mo <thimo@th7mo.nl>
2022-06-20 11:11:17 +0200

    fixed issue that check was inverted for compare bcrypt

--- account/Account.js
+++ account/Account.js
@@ -2,11 +2,11 @@ const { v4: uuidv4 } = require('uuid');
 
 module.exports = class Account {
 
-    constructor(id, username, emailAddress, hashedPassword) {
+    constructor(id, username, emailAddress, hashed_password) {
         this.id = id ? id : uuidv4()
         this.username = username;
         this.emailAddress = emailAddress;
-        this.hashedPassword = hashedPassword;
+        this.hashed_password = hashed_password;
     }
 
 }
--- spec/account/AccountController.test.js
+++ spec/account/AccountController.test.js
@@ -3,40 +3,50 @@ const accountController = new AccountController();
 const Account = require('../../account/Account');
 const BeforeEach = require('../support/BeforeEach');
 
-const testAccount = new Account(
-    null,
-    'dave',
-    's1127893@student.hsleiden.nl',
-    'Password'
-);
+const ValidationError = require('../../account/ValidationError');
+const AccountNotFoundError = require('../../account/AccountNotFoundError');
+
+const createTestAccount = () => {
+    return new Account(
+        null,
+        'dave',
+        's1127893@student.hsleiden.nl',
+        'Password'
+    );
+}
+
 
 describe('testing the createAccount() method of the AccountController', () => {
 
     it('should create an account successfully', async () => {
         await BeforeEach.run();
-        let accountRecords = await accountController.createAccount(testAccount);
+        let accountRecords = await accountController.createAccount(createTestAccount());
         expect(accountRecords.id.length > 0).toBeTrue();
 
     });
 
     it('should throw an error when email already exists', async () => {
         await BeforeEach.run();
-        await accountController.createAccount(testAccount);
-        await expectAsync(accountController.createAccount(testAccount)).toBeRejected();
+        await accountController.createAccount(createTestAccount());
+        await expectAsync(accountController.createAccount(createTestAccount())).toBeRejected();
     });
 });
 
 describe('testing the changeAccount() method of the AccountController', () => {
 
-    it('should create an account successfully', async () => {
+    it('should update password when correct password given', async () => {
         await BeforeEach.run();
-        let createdAccount = await accountController.createAccount(testAccount);
+        let createdAccount = await accountController.createAccount(createTestAccount());
         await expectAsync(
             accountController.changePassword(createdAccount, 'Password', 'newpassword')
         ).toBeResolved();
     });
 
-    it('should throw an error when email already exists', async () => {
-
+    it('should throw an error when password has lowercase difference', async () => {
+        await BeforeEach.run();
+        let createdAccount = await accountController.createAccount(createTestAccount());
+        await expectAsync(
+            accountController.changePassword(createdAccount, 'wrongpassword', 'newpassword')
+        ).toBeRejected();
     });
 });
