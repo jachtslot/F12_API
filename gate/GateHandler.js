a7ac4291497e290cacf50122d972b9289cc42d6e
th7mo <dev@th7mo.com>
2022-06-20 15:11:33 +0200

    protected endpoints to open gate

--- gate/GateHandler.js
+++ gate/GateHandler.js
@@ -1,10 +1,23 @@
 const GateController = require('./GateController');
 const gateController = new GateController();
+const AuthenticationHelper = require('../util/AuthenticationHelper');
+const RequestValidator = require('../util/RequestValidator');
+const Time = require('../util/Time');
 
-module.exports.openInnerGate = event => {
-    return gateController.openInnerGate(event);
+module.exports.openInnerGate = async event => {
+    const decodedToken = AuthenticationHelper.verifyToken(event);
+    const accountId = decodedToken.id;
+    const requestValidator = new RequestValidator(accountId, Time.now(), 1);
+    if (await requestValidator.hasAccess()) {
+        return gateController.openInnerGate(event);
+    }
 }
 
-module.exports.openOuterGate = event => {
-    return gateController.openOuterGate(event);
+module.exports.openOuterGate = async event => {
+    const decodedToken = AuthenticationHelper.verifyToken(event);
+    const accountId = decodedToken.id;
+    const requestValidator = new RequestValidator(accountId, Time.now(), 2);
+    if (await requestValidator.hasAccess()) {
+        return gateController.openOuterGate(event);
+    }
 }
