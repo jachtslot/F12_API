a8d15def926b996080284bb6f1493277dc4977e0
th7mo <thimo@th7mo.nl>
2022-06-20 09:53:50 +0200

    improved speed of unit tests

--- spec/account/AccountController.test.js
+++ spec/account/AccountController.test.js
@@ -1,32 +1,26 @@
-const databaseCredentials = require('../../util/DatabaseCredentials').CREDENTIALS;
 const AccountController = require('../../account/AccountController');
 const accountController = new AccountController();
 const Account = require('../../account/Account');
 const BeforeEach = require('../support/BeforeEach');
 
-const getTestAccount = () => {
-    return new Account(
-        null,
-        'dave',
-        's1127893@student.hsleiden.nl',
-        'Password'
-    )
-}
+const testAccount = new Account(
+    null,
+    'dave',
+    's1127893@student.hsleiden.nl',
+    'Password'
+);
 
 describe('testing the createAccount() method of the AccountController', () => {
 
     it('should create an account successfully', async () => {
         await BeforeEach.run();
-        const account = getTestAccount();
-
-        let accountRecords = await accountController.createAccount(account);
+        let accountRecords = await accountController.createAccount(testAccount);
         expect(accountRecords.id.length > 0).toBeTrue();
 
     })
 
     it('should throw an error when email already exists', async () => {
         await BeforeEach.run();
-        let testAccount = getTestAccount();
         await accountController.createAccount(testAccount);
         await expectAsync(accountController.createAccount(testAccount)).toBeRejected();
     })
