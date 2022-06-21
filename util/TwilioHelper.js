c47427fb3d434162286d8760c1e76ae94014fb8d
RamonK <ramonkrijnen10@outlook.com>
2022-06-21 22:50:11 +0200

    made Twilio calls async and moved logger activity to handler

--- util/TwilioHelper.js
+++ util/TwilioHelper.js
@@ -1,4 +1,3 @@
-const logger = require('../util/Logger');
 
 require('dotenv').config();
 
@@ -13,32 +12,14 @@ module.exports = class TwilioHelper {
         this.client = require('twilio')(this.accountSid, this.authToken);
     }
 
-    makeCall(event) {
-        this.client.calls.create({
+    async makeCall() {
+        await this.client.calls.create({
             twiml: '<Response><Pause length="1"/><Say>The gate will now open, please wait.</Say><Pause length="1"/></Response>',
             to: this.numberTo,
             from: this.numberFrom,
             timeout: 10,
             timeLimit: 5
         })
-            .then(() => {
-                logger.info(`Request with path: ${event.path} successful.`);
-                return {
-                    statusCode: 200,
-                    body: JSON.stringify({
-                        message: 'The outer gate will now open, please wait.'
-                    })
-                }
-            })
-            .catch(error => {
-                logger.error(`Error opening outer gate: Reason ${error}`)
-                return {
-                    statusCode: 500,
-                    body: JSON.stringify(
-                        'An error occurred. Please try again'
-                    )
-                }
-            });
     }
 }
 
